27f31d8bc7694ab2405323dbf412343dcc3d2d8d
Gang <gang.liu@tfidm.com>
2020-03-20 11:55:48 +0800

    add http2 support and proxy buffering env

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i -e "s|XX_REMOTE_URL_XX|$REMOTE_URL|g" /etc/nginx/conf.d/default.conf
-
+sed -i -e "s|XX_PROXY_BUFFERING_XX|$PROXY_BUFFERING|g" /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
